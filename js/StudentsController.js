2cebbf9938afbc67da8d30dfe3f148f1dc7520d7
David Garber <davidgarber@gmail.com>
2015-10-02 08:19:11 -0700

    Create student controller.

---
+++ js/StudentsController.js
@@ -0,0 +1,9 @@
+studentRoster.controller('SturentsCtrl', function StudentsCtrl($scope) {
+  $scope.students = [
+    { name: "Sam Smith"},
+    { name: "Dean Devlin"},
+    { name: "Stacy Marks"},
+    { name: "Fred Jones"},
+    { name: "Beth Parks"}
+  ]
+})
